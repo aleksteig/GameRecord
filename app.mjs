dc863b2de3eecb067d4fe9d16e525216619d294a
aleksteig <aleksteigen@gmail.com>
2025-04-08 14:23:58 +0200

    I believe I have finished what was asked for in task 4, as I now have the games array, as well as a file reading function that adds the read files to the local storage

--- app.mjs
+++ app.mjs
@@ -2,6 +2,7 @@ import Game from "./models/gameClass.mjs";
 
 let someRandomGame = new Game("Sudoku", "FromSoftware", "New York", "Nintendo", 3096, "2-5", "600 mins", "Easy", "https://youtube.com", 3000, 10);
 let someOtherRandomGame = new Game("Sudoku 2", "Reddit", "Norway", "Sega", 1024, "5", "120 mins", "Medium", "https://google.com", 10, 4);
+let games = [];
 
 const gameListKey = 'games'
 localStorage.clear();
@@ -40,15 +41,12 @@ function retrieveAllGamesFromStorage(){
     }
 }
 
-if(localStorage.length > 0){
-    console.log(retrieveAllGamesFromStorage());
-}
-
 function getAllSavedGamesAsJSON(){
     return JSON.parse(localStorage.getItem(gameListKey));
 }
 
 function importJSONFileToLocalStorage(JSONfile){
+    JSON.stringify(JSONfile);
     try{
         JSONfile = JSON.parse(JSONfile);
     }catch(error){
@@ -60,5 +58,50 @@ function importJSONFileToLocalStorage(JSONfile){
             tempListOfGames.push(JSONfile[i]);
         }
         localStorage.setItem(gameListKey, JSON.stringify(tempListOfGames));
+    }
+}
+
+const fileInput = document.getElementById("importSource");
+const messageDisplay = document.getElementById("message");
+
+fileInput.addEventListener("change", handleFileSelection);
+
+function  handleFileSelection(event){
+    const file = event.target.files[0];
+    const extension = file.name.split('.').pop();
+    messageDisplay.textContent = "";
+    
+    if(!file){
+        showMessage("No file was selected. Please choose a new file.", "error");
+        return;
+    } else if(extension != 'json'){
+        showMessage("Unsupported file type. Please use a json file.", "error");
+        return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = () => {
+        const readFile = reader.result;
+        importJSONFileToLocalStorage(readFile);
+        addGamesToGameList()
+    }
+    reader.onerror = () => {
+        showMessage("Error reading the file. Please try again.", "error");
+    }
+    reader.readAsText(file);
+}
+
+function showMessage(message, type){
+    messageDisplay.textContent = message;
+    messageDisplay.style.color = type === "error" ? "red" : "green";
+}
+
+function addGamesToGameList(){
+    const gamesInJSON = getAllSavedGamesAsJSON();
+    if(games.length < gamesInJSON.length){
+        games = [];
+        for(let i = 0; i < gamesInJSON.length; i++){
+            games.push(gamesInJSON[i]);
+        }
     }
 }
